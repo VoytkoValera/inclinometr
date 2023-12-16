617e025f1af63dda15444bf46e09e41c3ecbf567
Voytko Valerii <valeravoy@gmail.com>
2023-12-16 21:03:51 +0300

    edit main hello

--- inclinometr/main/main.c
+++ inclinometr/main/main.c
@@ -2,10 +2,11 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+
 void app_main(void)
 {
     while (true) {
-        printf("Hello from app_main!\n");
+        printf("Hello2 from app_main!\n");
         sleep(1);
     }
 }
